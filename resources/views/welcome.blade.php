d998c56eff6a04267c14888aeeff4bb059840b8b
unknown <ajy>
2020-07-02 00:50:00 +0800

    first

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>无锡芯相芯蚕丝被|定制蚕丝</title>
+    <meta charset = "utf-8">
+    <!-- 使用CSS样式来控制div布局 -->
+    <style type="text/css">
+        /*定义全局的div布局*/
+        div#global{
+            /*设置宽度，设置百分比可以根据浏览器自适应宽度*/
+            width: 100%;
+            /*设置高度，950px是设置是实体高度，不能自适应的*/
+            height: 800px;
+            /*设置背景颜色，方便区分布局*/
+        }
+        /*定义头部布局，前面的div可以不用写，#号前面默认有个id，凡事定义了id的标签，都可以用#号来表示*/
+        #heading{
+            width: 100%;
+            height: 300px;
+            /*background:url("static/part-00469-1490.jpg");*/
+        }
+        #content_menu{
+            width: 16%;
+            height: 700px;
+            /*定义浮动，不定义的话，界面会乱*/
+            float: left;
+        }
+        #content_body{
+            width: 84%;
+            height: 800px;
+            float: right;
+        }
+        #floor{
+            /*删除浮动，不然会被其他的浮动给遮住*/
+            clear: both;
+            height: 100px;
+
+        }
+
+        body{
+        }
+    </style>
+</head>
+<body >
+<!-- 在这里控制全局的页面，定义div的id是"global" -->
+<div id="global">
+    <div id="heading" >
+        <img src="\static\1593610319hw2414.png"height="300px"width="300px" style="float: left">
+        </br></br></br></br>
+        <p>蚕丝是自然界中集轻、柔、细为一体的天然纤维，素有"人体第二皮肤"的美誉，被业界称为"纤维皇后"。其主要成分为纯天然动物蛋白纤维，其构造和人类的皮肤是最相近的，有87%是一模一样的，内含多种人体必须的氨基酸，有防风、除湿、安神、滋养及平衡人体肌肤的功效。蚕丝滑爽、透气、轻柔、吸湿、不剌痒及抗静电等特点使其成为制作贴身衣物的上乘料子，而以蚕丝作为内质的蚕丝被更具有贴身保暖、蓬松轻柔、透气保健等得天独厚的品质和优点。古时候的达官贵人、皇亲国戚的被子，用的都是蚕丝被。所以，蚕丝被是盖起来最舒服的被子。</p>
+        <p>蚕丝是自然界中集轻、柔、细为一体的天然纤维，素有"人体第二皮肤"的美誉，被业界称为"纤维皇后"。其主要成分为纯天然动物蛋白纤维，其构造和人类的皮肤是最相近的，有87%是一模一样的，内含多种人体必须的氨基酸，有防风、除湿、安神、滋养及平衡人体肌肤的功效。蚕丝滑爽、透气、轻柔、吸湿、不剌痒及抗静电等特点使其成为制作贴身衣物的上乘料子，而以蚕丝作为内质的蚕丝被更具有贴身保暖、蓬松轻柔、透气保健等得天独厚的品质和优点。古时候的达官贵人、皇亲国戚的被子，用的都是蚕丝被。所以，蚕丝被是盖起来最舒服的被子。</p>
+    </div>
+    <div id="content_menu">
+        <p>  地址：江苏省无锡市民丰西苑192号</p>
+        <p>  联系方式:15906194531</p>
+    </div>
+    <div id="content_body">
+        <img src="\index\1.jpg"height="300px"width="280px">
+        <img src="\index\7.jpg"height="300px"width="280px">
+        <img src="\index\8.jpg"height="300px"width="280px">
+        <img src="\index\9.jpg"height="300px"width="280px">
+        <img src="\index\10.jpg"height="300px"width="280px">
+        <br/>
+        <img src="\index\2.jpg"height="300px"width="280px">
+        <img src="\index\3.jpg"height="300px"width="280px">
+        <img src="\index\4.jpg"height="300px"width="280px">
+        <img src="\index\5.jpg"height="300px"width="280px">
+        <img src="\index\6.jpg"height="300px"width="280px">
+    </div>
+    <div id="floor" style="text-align: center">
+        © 2020-2021 815807635.com , All Rights Reserved
+            <a href="http://icp.chinaz.com/info?q=%E6%A1%82ICP%E5%A4%8714001002%E5%8F%B7-1">赣ICP备20007132号-1</a>
+        </div>
+</div>
+</body>
+</html>
